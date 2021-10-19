e0e3602a89c750e442fb82cb043a1a81bf272c8c
advaitanilpatel <92788627+advaitanilpatel@users.noreply.github.com>
2021-10-19 16:51:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+student_array=[];
+function submit(){
+    var name1=document.getElementById("student1").value;
+    var name2=document.getElementById("student2").value;
+    var name3=document.getElementById("student3").value;
+    var name4=document.getElementById("student4").value;
+
+    student_array.push(name1);
+    student_array.push(name2);
+    student_array.push(name3);
+    student_array.push(name4);
+console.log(student_array);
+document.getElementById("display_array").innerHTML=student_array;
+document.getElementById("Submit").style.display="none";
+document.getElementById("Order").style.display="inline-block";
+
+}
+
+function sorting(){
+    student_array.sort();
+    document.getElementById("display_array").innerHTML=student_array;
+}
